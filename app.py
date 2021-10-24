252122a4be8753516459d91897dce38e5ea09701
Shaylan Dias <shaylandias@berkeley.edu>
2021-10-24 01:55:20 -0700

    fuck deploying

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ app = Flask(__name__, static_folder='./client/build', static_url_path='/')
 spotify_client_id = os.environ.get('SPOTIFY_CLIENT_ID')
 spotify_client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
-app.static_folder='./build'
-
 if os.environ.get('PRODUCTION') == 'TRUE':
     BASE_URL = ''
 else:
